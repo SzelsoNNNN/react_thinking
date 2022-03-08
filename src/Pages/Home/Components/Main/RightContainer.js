074fc9b38f94183e2fa0b4b2df648c47aeecf9fc
SzelsoNNNN <72217754+SzelsoNNNN@users.noreply.github.com>
2022-03-08 10:47:10 +0100

    Add files via upload

--- src/Pages/Home/Components/Main/RightContainer.js
+++ src/Pages/Home/Components/Main/RightContainer.js
@@ -3,17 +3,25 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
 import API_db from './../../../../API/db.json'
 import FilterContext from './../../filters'
 
 const RightContainer = () => {
     const [filters, setFilters] = useContext(FilterContext)
-        return(
-            <div className="main_rightContainer">
-                {API_db.map((book) => (
-                    <div className="book">
-                        {book.book_releaseMonth === (filters.filterMonth + 1) ? (
-                            <Card sx={{ m: 1, maxWidth: '100%'}}>
+    const books = []
+        API_db.map(book => {
+            if(book.book_releaseMonth === (filters.filterMonth + 1)) {
+                books.push(book)
+            }
+        })
+        if(books[0]) {
+            return(
+                <div className="main_rightContainer">
+                    {books.map((book) => (
+                        <Card sx={{ m: 1, width: '23.3%'}}>
                             <CardMedia
                                 component="img"
                                 height="140"
@@ -34,33 +42,20 @@ const RightContainer = () => {
                                 </Typography>
                             </CardContent>
                         </Card>
-                        ) : ''}
-                    </div>
-            ))}
-            </div>
-        )
+                ))}
+                </div>
+            )
+        } else {
+            return (
+                <Stack sx={{ width: '77.5%' }} spacing={3}>
+                    <Alert severity="info" color="secondary">
+                        <AlertTitle><strong>BŁĄD</strong></AlertTitle>
+                        Nie znaleziono żadnych wyników
+                    </Alert>
+                </Stack>
+            )
+        }
+
 }
 
 export default RightContainer
-
-{/* <Card sx={{ m: 1, width: 200, maxWidth: '30%'}}>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            width="auto"
-                            image={book.book_photography}
-                            alt={book.book_name}
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h7" component="b">
-                                {book.book_name}
-                            </Typography>
-                            <br />
-                            {book.book_isNew === true ? (<Typography gutterBottom variant="h7"component="b" color="secondary">
-                                NOWOŚĆ!
-                            </Typography>): "" }
-                            <Typography variant="body2" color="text.secondary">
-                                Data wydania: {book.book_releaseDay}/{book.book_releaseMonth}/{book.book_releaseYear}
-                            </Typography>
-                        </CardContent>
-                    </Card> */}
